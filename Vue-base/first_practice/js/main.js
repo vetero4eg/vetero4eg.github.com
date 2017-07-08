a9bbc9627612e38ed145ff3a2ec1f2907ab6e893
Olga Veter <olkaveter@gmail.com>
2017-07-09 00:35:09 +0300

    two lesson

---
+++ Vue-base/first_practice/js/main.js
@@ -0,0 +1,28 @@
+new Vue({
+    el: "#app",
+    data: {
+        message : "Hello Vue",
+        items: [
+            {
+                name: 'one',
+                status: false
+            },
+            {
+                name: 'two',
+                status: true
+            },
+            {
+                name: 'three',
+                status: false
+            },
+            {
+                name: 'four',
+                status: false
+            },
+            {
+                name: 'five',
+                status: false
+            }
+        ]
+    }
+})
