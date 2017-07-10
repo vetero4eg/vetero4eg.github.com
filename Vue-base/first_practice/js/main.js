425bc4c08df5bb52a32ba3bdecde8e88f2c70e23
Olga Veter <olkaveter@gmail.com>
2017-07-11 00:11:35 +0300

    refresh

--- Vue-base/first_practice/js/main.js
+++ Vue-base/first_practice/js/main.js
@@ -24,11 +24,21 @@ new Vue({
                 status: false
             }
         ],
-        show: false
+        show: false,
+        text: '',
+        limit: 140
     },
     methods: {
         onClick: function() {
             alert(this.message);
+        }
+    },
+    computed: {
+        count: function() {
+            return this.text.length;
+        },
+        disabled: function() {
+            return (this.count<this.limit) ? false : true;
         }
     }
 })
