d8a52fb9d7f1292491afc5d1e0afce8da4fb967d
Olga Veter <olkaveter@gmail.com>
2018-02-10 06:26:23 +0300

    TRL demo

---
+++ CitySelector/src/app.js
@@ -0,0 +1,10 @@
+const CitySelector = require('./CitySelector');
+
+/* Пример создания компонента:
+const citySelector = new CitySelector({
+    elementId: 'citySelector',
+    regionsUrl: 'http://localhost:3000/regions',
+    localitiesUrl: 'http://localhost:3000/localities',
+    saveUrl: 'http://localhost:3000/selectedRegions'
+});
+*/
---
+++ CitySelector/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    devtool: 'source-map',
+    entry: [
+        './src/app.js'
+    ],
+    output: {
+        path: path.join(__dirname, 'build'),
+        filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.css$/,
+                loaders: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.less$/,
+                loaders: ['style-loader', 'css-loader', 'less-loader']
+            }
+        ]
+    }
+};
