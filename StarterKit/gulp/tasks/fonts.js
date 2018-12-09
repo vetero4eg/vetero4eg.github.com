5f42f821aae2c823da149437b71f5d04d0aeb22d
softex-veter <o.veter@softex-group.com>
2018-12-09 13:15:49 +0300

    update

--- StarterKit/gulp/tasks/fonts.js
+++
@@ -1,6 +0,0 @@
-module.exports = function() {
-    $.gulp.task('fonts', () => {
-        return $.gulp.src('./dev/fonts/**/*.*')
-            .pipe($.gulp.dest('./build/fonts/'));
-    });
-};
--- StarterKit/gulp/tasks/watch.js
+++
@@ -1,8 +0,0 @@
-module.exports = function () {
-    $.gulp.task('watch', function () {
-        $.gulp.watch('./dev/**/*.pug', $.gulp.series('pug'));
-        $.gulp.watch('./dev/**/*.sass', $.gulp.series('styles:dev'));
-        $.gulp.watch('./dev/img/svg/*.svg', $.gulp.series('svg'));
-        $.gulp.watch(['./dev/js/main.js'], $.gulp.series('js:dev'));
-    });
-};
